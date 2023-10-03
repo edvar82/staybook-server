74fff8a117d0ed575df45bb319b376682c1492ca
Anthony D <anthonydantas15@gmail.com>
2023-10-03 14:12:37 -0400

    fix: mudanca rota de login de get para post

--- src/routes/clienteRouter.js
+++ src/routes/clienteRouter.js
@@ -4,6 +4,6 @@ const clientController = require("../controllers/clienteController");
 const router = express.Router();
 
 router.post("/", clientController.createCliente);
-router.get("/", clientController.loginCliente);
+router.post("/login", clientController.loginCliente);
 
 module.exports = router;
